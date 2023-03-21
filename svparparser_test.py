54d60463676d40ba9f92ea04cb5ed7283df1e26c
Nikkitachka <Nikkitachka@yandex.ru>
2023-03-21 16:03:16 +0300

    Рабочая версия

--- svparparser_test.py
+++ svparparser_test.py
@@ -6,7 +6,7 @@ import SVParParser as svpp
 parser = svpp.SVParParser("test_module_1.sv")
 parser.head_parse()
 # parser.parse_log()
-# parser.parse_file_log()
+parser.parse_file_log()
 # parser = svpp.SVParParser("test_module_2.sv")
 # parser.parse()
 # parser.parse_log()clearcl
